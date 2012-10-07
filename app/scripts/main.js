3b16edac1d674aab01b541ac1457badcda100138
Willie Dulin <willie@kingsburytx.com>
2012-10-07 14:11:03 -0400

    ruled paper background

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -25,6 +25,32 @@ function shadowOff(ctx) {
     ctx.shadowBlur = 0;
 }
     
+    
+function drawBackground() { // Ruled paper
+    var STEP_Y = 12,
+    TOP_MARGIN = STEP_Y * 4,
+    LEFT_MARGIN = STEP_Y * 3,
+    i = context.canvas.height;
+    // Horizontal lines
+    context.strokeStyle = 'lightgray';
+    context.lineWidth = 0.5;
+    while(i > TOP_MARGIN) {
+        context.beginPath();
+        context.moveTo(0, i);
+        context.lineTo(context.canvas.width, i);
+        context.stroke();
+        i -= STEP_Y;
+    }
+    // Vertical line
+    context.strokeStyle = 'rgba(100,0,0,0.3)';
+    context.lineWidth = 1;
+    context.beginPath();
+    context.moveTo(LEFT_MARGIN,0);
+    context.lineTo(LEFT_MARGIN,context.canvas.height);
+    context.stroke();
+}       
+    
+    
 function drawTitle(txt) {
      var TEXT_X = canvas.width/2,
          TEXT_Y = 45;
@@ -43,6 +69,7 @@ function drawTitle(txt) {
     
 function draw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
+    drawBackground();
     drawTitle(title);
 }
 
